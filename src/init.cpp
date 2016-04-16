fe941c847fa8c340ffc5828f5da0ecce2954370b
Pieter Wuille <pieter.wuille@gmail.com>
2016-04-16 12:25:12 +0200

    Always require OS randomness when generating secret keys

--- src/init.cpp
+++ src/init.cpp
@@ -1748,8 +1748,6 @@ bool AppInit2()
 
         if (fFirstRun) {
             // Create new keyUser and set as default key
-            RandAddSeedPerfmon();
-
             CPubKey newDefaultKey;
             if (pwalletMain->GetKeyFromPool(newDefaultKey)) {
                 pwalletMain->SetDefaultKey(newDefaultKey);
@@ -2008,8 +2006,6 @@ bool AppInit2()
 
     if (!strErrors.str().empty())
         return InitError(strErrors.str());
-
-    RandAddSeedPerfmon();
 
     //// debug print
     LogPrintf("mapBlockIndex.size() = %u\n", mapBlockIndex.size());
