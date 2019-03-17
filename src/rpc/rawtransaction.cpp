eons-finance/ALQO

add empty output to createrawzerocoinstake

src/rpc/rawtransaction.cpp
@@ -957,12 +957,12 @@ UniValue createrawzerocoinstake(const UniValue& params, bool fHelp)
 
     // create the zerocoinmint output (one spent denom + three 1-zPIV denom)
     libzerocoin::CoinDenomination staked_denom = input_mint.GetDenomination();
-    std::vector<CTxOut> vOutMint(4);
+    std::vector<CTxOut> vOutMint(5);
     CDeterministicMint dMint;
-    if (!pwalletMain->CreateZPIVOutPut(staked_denom, vOutMint[0], dMint))
+    if (!pwalletMain->CreateZPIVOutPut(staked_denom, vOutMint[1], dMint))
         throw JSONRPCError(RPC_WALLET_ERROR, "failed to create new zpiv output");
 
-    for (int i=1; i<4; i++) {
+    for (int i=2; i<5; i++) {
         if (!pwalletMain->CreateZPIVOutPut(libzerocoin::ZQ_ONE, vOutMint[i], dMint))
             throw JSONRPCError(RPC_WALLET_ERROR, "failed to create new zpiv output");
     }

zeroedge83/wagerr

Signed-off-by: wagerrtor <tor@i2pmail.org>

src/rpc/rawtransaction.cpp
@@ -971,12 +971,12 @@ UniValue createrawzerocoinstake(const UniValue& params, bool fHelp)
 
     // create the zerocoinmint output (one spent denom + three 1-zWGR denom)
     libzerocoin::CoinDenomination staked_denom = input_mint.GetDenomination();
-    std::vector<CTxOut> vOutMint(4);
+    std::vector<CTxOut> vOutMint(5);
     CDeterministicMint dMint;
-    if (!pwalletMain->CreateZWGROutPut(staked_denom, vOutMint[0], dMint))
+    if (!pwalletMain->CreateZWGROutPut(staked_denom, vOutMint[1], dMint))
         throw JSONRPCError(RPC_WALLET_ERROR, "failed to create new zwgr output");
 
-    for (int i=1; i<4; i++) {
+    for (int i=2; i<5; i++) {
         if (!pwalletMain->CreateZWGROutPut(libzerocoin::ZQ_ONE, vOutMint[i], dMint))
             throw JSONRPCError(RPC_WALLET_ERROR, "failed to create new zwgr output");
     }