7ddc58986b2096c6e80c724e2ec8f2682c0c93f4
Kokary <kokary@wagerr.com>
2019-04-30 12:13:16 +0200

    Move nBetValue out of CTxOut and clean up vExpectedPayouts

--- src/primitives/transaction.h
+++ src/primitives/transaction.h
@@ -116,7 +116,6 @@ class CTxOut
 {
 public:
     CAmount nValue;
-    CAmount nBetValue;
     CScript scriptPubKey;
     int nRounds;
 
@@ -126,7 +125,6 @@ public:
     }
 
     CTxOut(const CAmount& nValueIn, CScript scriptPubKeyIn);
-    CTxOut(const CAmount& nValueIn, CScript scriptPubKeyIn, const CAmount& nBetValueIn);
     
     ADD_SERIALIZE_METHODS;
 
@@ -139,7 +137,6 @@ public:
     void SetNull()
     {
         nValue = -1;
-        nBetValue = -1;
         scriptPubKey.clear();
         nRounds = -10; // an initial value, should be no way to get this by calculations
     }
