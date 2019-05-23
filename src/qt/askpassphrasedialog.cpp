observerdev/obsr

Add basic GUI voting tab to the wallet
Signed-off-by: observerdev <dev@obsr.org>

src/qt/askpassphrasedialog.cpp
@@ -76,7 +76,7 @@ AskPassphraseDialog::AskPassphraseDialog(Mode mode, QWidget* parent, WalletModel
     }
 
     // Set checkbox "For anonymization, automint, and staking only" depending on from where we were called
-    if (context == Context::Unlock_Menu || context == Context::Mint_zOBSR || context == Context::BIP_38) {
+    if (context == Context::Unlock_Menu || context == Context::Mint_zOBSR || context == Context::BIP_38 || context == Context::UI_Vote) {
         ui->anonymizationCheckBox->setChecked(true);
     }
     else {

zeroedge83/wagerr

Signed-off-by: wagerrtor <tor@i2pmail.org>

src/qt/askpassphrasedialog.cpp
@@ -76,7 +76,7 @@ AskPassphraseDialog::AskPassphraseDialog(Mode mode, QWidget* parent, WalletModel
     }
 
     // Set checkbox "For anonymization, automint, and staking only" depending on from where we were called
-    if (context == Context::Unlock_Menu || context == Context::Mint_zWGR || context == Context::BIP_38) {
+    if (context == Context::Unlock_Menu || context == Context::Mint_zWGR || context == Context::BIP_38 || context == Context::UI_Vote) {
         ui->anonymizationCheckBox->setChecked(true);
     }
     else {