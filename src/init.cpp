6679d225a7536a0298f2d312b044a865105894e6
warrows <warrows@gamer666.fr>
2019-08-28 17:40:25 +0200

    Remove unused OpenSSL includes

--- src/init.cpp
+++ src/init.cpp
@@ -65,7 +65,6 @@
 #include <boost/interprocess/sync/file_lock.hpp>
 #include <boost/thread.hpp>
 #include <boost/foreach.hpp>
-#include <openssl/crypto.h>
 
 #if ENABLE_ZMQ
 #include "zmq/zmqnotificationinterface.h"
