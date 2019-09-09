8b99e8603eaa707682ee480ad6eb9527a245ca53
random-zebra <random.zebra@protonmail.com>
2019-09-09 11:39:41 +0200

    Update missing headers
    Update copyright headers for:
    - PPCoin
    - Novacoin
    - BlackCoin

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -1,4 +1,6 @@
-// Copyright (c) 2012-2013 The PPCoin developers
+// Copyright (c) 2011-2013 The PPCoin developers
+// Copyright (c) 2013-2014 The NovaCoin Developers
+// Copyright (c) 2014-2018 The BlackCoin Developers
 // Copyright (c) 2015-2018 The PIVX developers
 // Copyright (c) 2018 The Wagerr developers
 // Distributed under the MIT/X11 software license, see the accompanying
