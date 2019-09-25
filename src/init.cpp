29e4c52f23fac1f1d89eb1281533e57ef18537dc
random-zebra <random.zebra@protonmail.com>
2019-09-25 03:44:46 +0200

    Define messageSigner and hashSigner classes
    and remove obfuscationSigner class.
    Move IsVinAssociatedWithPubkey to CMasternodeClass and rename it
    IsInputAssociatedWithPubkey.

--- src/init.cpp
+++ src/init.cpp
@@ -28,6 +28,7 @@
 #include "masternode-payments.h"
 #include "masternodeconfig.h"
 #include "masternodeman.h"
+#include "messagesigner.h"
 #include "miner.h"
 #include "net.h"
 #include "rpc/server.h"
@@ -1963,7 +1964,7 @@ bool AppInit2()
             CKey key;
             CPubKey pubkey;
 
-            if (!obfuScationSigner.SetKey(strMasterNodePrivKey, errorMessage, key, pubkey)) {
+            if (!CMessageSigner::GetKeysFromSecret(strMasterNodePrivKey, key, pubkey)) {
                 return InitError(_("Invalid masternodeprivkey. Please see documenation."));
             }
 
