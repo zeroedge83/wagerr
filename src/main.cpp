d192ba3ef3e7a2079a9e537e0c3d96679f64ea56
Kokary <kokary@wagerr.com>
2020-05-12 10:44:39 +0200

    Replace boost::lexical_cast with std::string

--- src/main.cpp
+++ src/main.cpp
@@ -6458,7 +6458,7 @@ bool static ProcessMessage(CNode* pfrom, std::string strCommand, CDataStream& vR
                     return true;
                 }
 
-                std::string strMessage = tx.GetHash().ToString() + boost::lexical_cast<std::string>(sigTime);
+                std::string strMessage = tx.GetHash().ToString() + std::to_string(sigTime);
 
                 std::string errorMessage = "";
                 if (!obfuScationSigner.VerifyMessage(pmn->pubKeyMasternode, vchSig, strMessage, errorMessage)) {
