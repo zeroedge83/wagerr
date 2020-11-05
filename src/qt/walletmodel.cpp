furszy/bitcoin-core

zerocoin spend flow improved, multi outputs accepted

src/qt/walletmodel.cpp
@@ -435,6 +435,36 @@ bool WalletModel::mintCoins(CAmount value, CCoinControl* coinControl ,std::strin
     return strError.empty();
 }
 
+bool WalletModel::sendZpiv(
+        vector<CZerocoinMint> &vMintsSelected,
+        bool fMintChange,
+        bool fMinimizeChange,
+        CZerocoinSpendReceipt &receipt,
+        std::list<std::pair<CBitcoinAddress*, CAmount>> outputs,
+        std::string changeAddress
+        ){
+
+    CBitcoinAddress *changeAdd = (!changeAddress.empty()) ? new CBitcoinAddress(changeAddress) : nullptr;
+    CAmount value = 0;
+    for(std::pair<CBitcoinAddress*, CAmount> pair : outputs){
+        value += pair.second;
+    }
+
+    CWalletTx wtxNew;
+    return wallet->SpendZerocoin(
+            value,
+            100,
+            wtxNew,
+            receipt,
+            vMintsSelected,
+            fMintChange,
+            fMinimizeChange,
+            outputs,
+            changeAdd
+    );
+
+}
+
 bool WalletModel::convertBackZpiv(
         CAmount value,
         vector<CZerocoinMint> &vMintsSelected,
@@ -459,6 +489,7 @@ bool WalletModel::convertBackZpiv(
             vMintsSelected,
             fMintChange,
             fMinimizeChange,
+            std::list<std::pair<CBitcoinAddress*, CAmount>>(),
             &addressTo
     );
 }

zeroedge83/wagerr

src/qt/walletmodel.cpp
@@ -487,6 +487,36 @@ WalletModel::SendCoinsReturn WalletModel::placeBet(WalletModelTransaction& trans
     return SendCoinsReturn(OK);
 }
 
+bool WalletModel::sendZwgr(
+        vector<CZerocoinMint> &vMintsSelected,
+        bool fMintChange,
+        bool fMinimizeChange,
+        CZerocoinSpendReceipt &receipt,
+        std::list<std::pair<CBitcoinAddress*, CAmount>> outputs,
+        std::string changeAddress
+        ){
+
+    CBitcoinAddress *changeAdd = (!changeAddress.empty()) ? new CBitcoinAddress(changeAddress) : nullptr;
+    CAmount value = 0;
+    for(std::pair<CBitcoinAddress*, CAmount> pair : outputs){
+        value += pair.second;
+    }
+
+    CWalletTx wtxNew;
+    return wallet->SpendZerocoin(
+            value,
+            100,
+            wtxNew,
+            receipt,
+            vMintsSelected,
+            fMintChange,
+            fMinimizeChange,
+            outputs,
+            changeAdd
+    );
+
+}
+
 bool WalletModel::convertBackZwgr(
         CAmount value,
         vector<CZerocoinMint> &vMintsSelected,
@@ -511,6 +541,7 @@ bool WalletModel::convertBackZwgr(
             vMintsSelected,
             fMintChange,
             fMinimizeChange,
+            std::list<std::pair<CBitcoinAddress*, CAmount>>(),
             &addressTo
     );
 }